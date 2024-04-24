5622e7bd43248b2fffecaaf89345acf3fa8f5254
Yudain22 <di4046@gmail.com>
2024-04-24 17:14:24 +0900

    first commit

---
+++ src/main/java/org/zerock/springboot/repository/search/BoardSearch.java
@@ -0,0 +1,10 @@
+package org.zerock.springboot.repository.search;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.zerock.springboot.domain.Board;
+
+public interface BoardSearch {
+    Page<Board> search1(Pageable pageable);
+    Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
+}
