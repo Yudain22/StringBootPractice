36bc69e5e5ccdd19709b046ed4f9b1793b0f7635
Yudain22 <di4046@gmail.com>
2024-04-25 16:14:38 +0900

    240425 7교시 조회처리와 화면개발

--- src/main/java/org/applicationtest/springboot/controller/BoardController.java
+++ src/main/java/org/applicationtest/springboot/controller/BoardController.java
@@ -53,5 +53,12 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
+    @GetMapping("/read")
+    public void read(Long bno,PageRequestDTO pageRequestDTO, Model model) {
+        BoardDTO boardDTO = boardService.readOne(bno);
+        log.info(boardDTO);
+        model.addAttribute("dto", boardDTO);
+    }
+
 
 }
