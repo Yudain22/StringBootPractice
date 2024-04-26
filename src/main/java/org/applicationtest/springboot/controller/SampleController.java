fc9819f886af0ae84d754aa4786e784cc997520e
Yudain22 <di4046@gmail.com>
2024-04-26 15:14:20 +0900

    list에서 등록, 삭제, 수정, 상세보기 구현

--- src/main/java/org/applicationtest/springboot/controller/SampleController.java
+++ src/main/java/org/applicationtest/springboot/controller/SampleController.java
@@ -1,13 +1,20 @@
 package org.applicationtest.springboot.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.applicationtest.springboot.dto.BoardDTO;
 import org.applicationtest.springboot.dto.NoticeDTO;
 import org.applicationtest.springboot.dto.PageRequestDTO;
 import org.applicationtest.springboot.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -17,7 +24,10 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
+@RequestMapping("/ex")
 @Log4j2
+@RequiredArgsConstructor
+
 public class SampleController {
     @Autowired
     private NoticeService noticeService;
@@ -28,7 +38,7 @@ public class SampleController {
         model.addAttribute("msg", "hello world");
     }
 
-    @GetMapping("/ex/ex1")
+    @GetMapping("/ex1")
     public void ex1(Model model) {
         List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
         model.addAttribute("list", list);
@@ -50,7 +60,7 @@ public class SampleController {
         }
     }
 
-    @GetMapping("/ex/ex2")
+    @GetMapping("/ex2")
     public void ex2(Model model) {
         List<String> strList = IntStream.range(1,10)
                 .mapToObj(i -> "Data" + i)
@@ -71,47 +81,91 @@ public class SampleController {
         model.addAttribute("dto", sampleDTO);
     }
 
-    @GetMapping("/ex/ex3")
+    @GetMapping("/ex3")
     public void ex3(Model model) {
         model.addAttribute("arr", new String[]{"AAA", "BBB", "CCC"});
     }
 
-    @GetMapping("/ex/notice_list")
+    @GetMapping("/notice_list")
     public void notice_list(PageRequestDTO pageRequestDTO, Model model) {
         model.addAttribute("noticeList",noticeService.list(pageRequestDTO));
     }
 
-    @GetMapping("/ex/notice_add")
-    public void notice_add(Model model) {
-        model.addAttribute("arr", new String[]{"AAA", "BBB", "CCC"});
+    @GetMapping("/notice_add")
+    public void addGET() {
+
     }
 
-    @GetMapping("/ex/notice_view")
-    public void notice_view(Long no,Model model) {
+    @PostMapping("/notice_add")
+    public String addPOST(@Valid NoticeDTO noticeDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        log.info("notice Post register......");
+
+        if (bindingResult.hasErrors()) {
+            log.info("has errors......");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/ex/notice_add";
+        }
+        log.info(noticeDTO);
+        Long no = noticeService.register(noticeDTO);
+        redirectAttributes.addFlashAttribute("result",no);
+        return "redirect:/ex/notice_list";
+    }
+
+    @GetMapping({"/notice_view","/notice_modify"})
+    public void view(Long no,Model model,PageRequestDTO pageRequestDTOd) {
         model.addAttribute("notice", noticeService.readOne(no));
     }
 
-    @GetMapping("/ex/login")
+    @PostMapping("/notice_modify")
+    public String modify(PageRequestDTO pageRequestDTO,
+                         @Valid NoticeDTO noticeDTO,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes) {
+        log.info("notice Modify post......"+noticeDTO);
+
+        if (bindingResult.hasErrors()) {
+            log.info("has errors......");
+            String link = pageRequestDTO.getLink();
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            redirectAttributes.addAttribute("no", noticeDTO.getNo());
+            return "redirect:/ex/notice_modify?"+link;
+        }
+
+        noticeService.modify(noticeDTO);
+        redirectAttributes.addFlashAttribute("result","modified");
+        redirectAttributes.addAttribute("no",noticeDTO.getNo());
+        return "redirect:/ex/notice_view";
+    }
+
+    @PostMapping("/notice_remove")
+    public String remove(Long no,RedirectAttributes redirectAttributes) {
+        log.info("remove post..." + no);
+        noticeService.remove(no);
+        redirectAttributes.addFlashAttribute("result","removed");
+        return "redirect:/ex/notice_list";
+    }
+
+    @GetMapping("/login")
     public void login(Model model) {
 
     }
 
-    @GetMapping("/ex/join")
+    @GetMapping("/join")
     public void join(Model model) {
 
     }
 
-    @GetMapping("/example/index")
+    @GetMapping("/index")
     public void index(Model model) {
 
     }
 
-    @GetMapping("/example/mypage")
+    @GetMapping("/mypage")
     public void mypage(Model model) {
 
     }
 
-    @GetMapping("/example/program")
+    @GetMapping("/program")
     public void program(Model model) {
 
     }
